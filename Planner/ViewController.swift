7a2f1db880ff579b65c0731aa2be86fcca57293a
Tarek Nabulsi <tareknabulsi@tareks-macbook-pro.local>
2019-01-04 13:40:40 -0600

    Initial Commit

---
+++ Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planner
+//
+//  Created by Tarek Nabulsi on 1/4/19.
+//  Copyright © 2019 Tarek Nabulsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
